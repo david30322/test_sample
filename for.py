701824b2b24c21343d6c73701220028ffeddaead
david <mswzhg@126.com>
2019-06-01 22:41:32 +0800

    first commit

---
+++ for.py
@@ -0,0 +1,11 @@
+names = ['david', 'micheal', 'seven']
+for name in names:
+   print 'hello',name,'!'
+sum = 0
+for x in range(101):
+    y = x%12
+    print y
+    if y == 9:
+        sum += 1 
+print 'sun=',sum
+
