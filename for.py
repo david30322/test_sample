e9ddd41b4fcf25ff804005673ba2d585b56ba592
david30322 <ms0500830322>
2019-06-02 14:00:11 +0800

    add a new line

--- for.py
+++ for.py
@@ -3,9 +3,9 @@ for name in names:
    print 'hello',name,'!'
 sum = 0
 for x in range(101):
-#    y = x%12
+    y = x%12
 #   print y
     if y == 9:
         sum += 1 
 print 'sun=',sum
-
+print 'the whole world'
